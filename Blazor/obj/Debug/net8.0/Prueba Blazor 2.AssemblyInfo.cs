2ec24c5dc2e225d7564bc5580e14e2591eaf9116
arobledo10 <luismarob@gmail.com>
2024-03-12 21:03:27 -0600

    app

--- Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
+++ Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c11cd0edf9f9c35e2cfcaa70441c3d863684297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aebac65e82ecaacb468809229b4e84e4b1300ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
