ba91c5b5df287903467ac2de58ed31ead07e1132
KuzeDavid <pain.swordartonline@gmail.com>
2024-03-14 01:33:48 -0600

    Revert "Mas modificaciones de horarios y perfil"
    This reverts commit 9f0e112c260e70e9cbde9551ce25ec83a1a1bd44.

--- Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
+++ Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+600b7a89c3ec8c1ddb66567db5fc27a4fc65ef82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10bcf39816d98bd1b4f19136308ed81dcbf061ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
