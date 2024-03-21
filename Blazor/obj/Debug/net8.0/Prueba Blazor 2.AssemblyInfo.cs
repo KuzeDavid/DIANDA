0463c68f0311fb9596ed387194484972f7b2bc25
KuzeDavid <pain.swordartonline@gmail.com>
2024-03-21 01:36:44 -0600

    Recuperar contraseña

--- Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
+++ Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7e6c817f719b2e8490754eda53b5752d646d44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea3af729f72a79781773b5ef98c48ebe06c44f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
