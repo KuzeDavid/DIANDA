25b974011071830120271572bb3c21b201c3ad41
KuzeDavid <pain.swordartonline@gmail.com>
2024-04-07 17:20:00 -0600

    Mas bomneto y arreglado GestionProfe Servicio jala al toque, nuevo componente

--- Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
+++ Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c9fb292499ab983700f9236bea329a1b17748f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d96e652d21f2cab46dc3acc0a0d2def8c2b4a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
