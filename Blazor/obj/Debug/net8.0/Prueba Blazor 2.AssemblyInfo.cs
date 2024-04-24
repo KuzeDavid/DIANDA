2dbe53b9a4d203d867e5f3674eccc150a0d75742
KuzeDavid <pain.swordartonline@gmail.com>
2024-04-23 19:49:52 -0600

    Codigo limpio preimplementacion de notificacion

--- Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
+++ Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6635a671547f3a1ee8140223ad7a94cca0b02f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6968bb7dfafdcedac371e2028dfab68d6d277b69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
