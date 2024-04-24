746a39baffd6c4a218d24a6e82260ff6d9d453b5
KuzeDavid <pain.swordartonline@gmail.com>
2024-04-23 21:20:51 -0600

    Notificacion en proceso

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Prueba_Blazor_2;
+using Blazored.Toast;
 using Radzen;
 using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,7 +14,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddBlazoredToast();
 builder.Services.AddRadzenComponents();
+
 
 builder.Services.AddSingleton<NavegacionService>();
 
--- Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
+++ Blazor/obj/Debug/net8.0/Prueba Blazor 2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6968bb7dfafdcedac371e2028dfab68d6d277b69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dbe53b9a4d203d867e5f3674eccc150a0d75742")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba Blazor 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
